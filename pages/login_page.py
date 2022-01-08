a624b21c1a65c1baa7c9b1d2016b9020b3cdf55d
anastasia-uchaneva-qa <nastya.uchaneva@gmail.com>
2022-01-08 19:02:24 +0300

    did changes in tests for Login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -12,7 +12,7 @@ class LoginPage(BasePage):
         assert "login" in self.browser.current_url
 
     def should_be_login_form(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_FORM), "Login form is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        assert self.is_element_present(*MainPageLocators.REGISTRATION_FORM), "Registration form is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not presented"
