d48f58281c65addb3a2758e278f0df2c018fe039
anastasia-uchaneva-qa <nastya.uchaneva@gmail.com>
2022-01-09 18:04:33 +0300

    small changes

--- test_product_page.py
+++ test_product_page.py
@@ -52,8 +52,6 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
 
 @pytest.mark.add_to_basket
 class TestUserAddToBasketFromProductPage():
