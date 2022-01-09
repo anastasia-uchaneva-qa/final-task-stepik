a3a45e0159237b78ac5c59dfcf0b878ae03383d2
anastasia-uchaneva-qa <nastya.uchaneva@gmail.com>
2022-01-09 10:42:38 +0300

    parametrization for test was added on basket page, and xfail was applied

--- test_product_page.py
+++ test_product_page.py
@@ -1,42 +1,18 @@
 import time
+import pytest
 from pages.product_page import ProductPage
 
-def test_quiz(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_basket()
-    page.solve_quiz_and_get_code()
-
-def test_user_should_see_successful_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+#link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+#@pytest.mark.parametrize('promo_offer', ["0","1","2", "3", "4", "5", "6", "7", "8", "9"])
+@pytest.mark.parametrize('promo_offer', ["0","1","2", "3", "4", "5", "6", "8", "9", pytest.param("7", marks=pytest.mark.xfail)])
+def test_quiz(browser, promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_successful_message()
-
-def test_user_should_see_message_with_price(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_basket()
-    page.solve_quiz_and_get_code()
     page.should_be_message_with_price()
-
-def test_check_price(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_basket()
-    page.solve_quiz_and_get_code()
     page.check_price()
-
-def test_check_name_of_book(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_basket()
-    page.solve_quiz_and_get_code()
     page.check_name_of_book()
     #time.sleep(60)
